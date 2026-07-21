1598908284a98a2a8c97afa4f6acb2955d87e0f9
Diego Geymonat <dgeymonat84@gmail.com>
2026-07-21 08:25:01 -0300

    Change structure format

---
+++ ApiLabNext.Api/Controllers/TestingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiLabNext.Api.Controllers
+{
+    [ApiController]
+    [Route("api/testing")]
+    public class TestingController : ControllerBase
+    {
+        [HttpGet("run-all")]
+        public IActionResult RunAll()
+        {
+            return Ok("Running all endpoints...");
+        }
+    }
+}
